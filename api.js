fcebc182931b76a55f7296c13e8071f8e25b0487
andreamogren <andrea.mogren91@gmail.com>
2016-09-29 09:22:14 +0200

    Update api.js

--- api.js
+++ api.js
@@ -86,9 +86,7 @@ function getData() {
 
    
    console.log(found);
-   document.getElementById('output1').innerHTML = ("Name: "+ found.name +
-    "<br>Height: " +found.height+"<br>Eye color: "+found.eye_color+
-    "<br>Weight: "+found.mass+"<br>Birth year: "+found.birth_year+"<br>Gender: "+found.gender);
+   document.getElementById('output1').innerHTML = ("Name: "+ found.name +"<br>Height: " +found.height+"<br>Eye color: "+found.eye_color);
   
   });
 }
@@ -97,45 +95,20 @@ function getData() {
    // do jQuery
    var entry = "";
    var starwars2;
-  $.get('http://swapi.co/ap/planets', function(response) {
-   starwars2 = response.results;
-   var query = entry;
-   var found;
-   for (var i = 0; i <= starwars2.length; i++) {
-    if (starwars2[i] && starwars2[i].name.indexOf(query) > -1) {
-      found = starwars2[i];
-    }
-   }
-   
-   
-   console.log(found);
-   document.write("Name: "+ found.name +"<br>Climate: " +found.climate);
-  });
+	$.get('http://swapi.co/ap/planets', function(response) {
+	 starwars2 = response.results;
+	 var query = entry;
+	 var found;
+	 for (var i = 0; i <= starwars2.length; i++) {
+	 	if (starwars2[i] && starwars2[i].name.indexOf(query) > -1) {
+	 		found = starwars2[i];
+	 	}
+	 }
+	 
+	 
+	 console.log(found);
+	 document.write("Name: "+ found.name +"<br>Climate: " +found.climate);
+	});
 });*/
-
-/*
-Random Image Script- By JavaScript Kit (http://www.javascriptkit.com) 
-Over 400+ free JavaScripts here!
-Keep this notice intact please
-*/
-
-function random_imglink(){
-var myimages=new Array()
-//specify random images below. You can have as many as you wish
-myimages[1]="http://67.media.tumblr.com/7c53a0454aa607ef4cec820972961e69/tumblr_o5fiynvcm41rlapeio1_540.gif"
-myimages[2]="http://bestanimations.com/Sci-Fi/StarWars/yoda-sat-wars-animated-gif-3.gif"
-myimages[3]="http://i.imgur.com/Fpkz2Va.gif"
-myimages[4]="https://media.giphy.com/media/vt4gALQsxCDmM/giphy.gif"
-myimages[5]="https://media.giphy.com/media/EXwLWGViGKevm/giphy.gif"
-myimages[6]="http://i4.photobucket.com/albums/y145/MoonlightStorm/gifs/gifleiashutup.gif"
-
-var ry=Math.floor(Math.random()*myimages.length)
-if (ry==0)
-ry=1
-document.write('<img src="'+myimages[ry]+'" border=0>')
-}
-random_imglink()
-//-->
-
 
 
